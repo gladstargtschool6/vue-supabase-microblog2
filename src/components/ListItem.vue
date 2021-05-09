cae99435699120addc2a73ef10539a9684286309
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-09 07:35:52 +0100

    Update ListItem.vue

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -17,7 +17,7 @@
           >
             Read the article
             <svg
-              xmlns="/http://www.w3.org/2000/svg"
+              xmlns="http://www.w3.org/2000/svg"
               class="w-5 h-5 ml-1"
               viewBox="0 0 24 24"
               fill="none"
