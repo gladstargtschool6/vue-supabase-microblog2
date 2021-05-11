e4234110957a8dd890fd6889f157e422fb151f45
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-11 05:31:43 +0100

    Update List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -25,7 +25,7 @@ export default {
   data: () => ({
     posts: [],
     database: null,
-  }),.
+  }),
   created() {
     const database = new DatabaseService();
     this.database = database.getInstance();
