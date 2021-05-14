e871a3d7ff29bf11f685875b771467afb23d03c8
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-14 15:06:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router';
-import store from './store';
-
-axios.defaults.withCredentials = true
-axios.defaults.baseURL = 'https://imdvvfcbtrfewysxgrnr.supabase.co/';
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+
 new Vue({
-  store,
   router,
-  render: h => h(App)
+  store,
+  render: function (h) { return h(App) }
 }).$mount('#app')
