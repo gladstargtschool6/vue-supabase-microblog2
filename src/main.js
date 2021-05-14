72c0bcdace11f17ca5cf1a1dadc678cce8c7ec1a
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-14 22:19:46 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import auth from './src/store/mosules/auth.js'
+import auth from './src/store/modules/auth.js'
 
 Vue.config.productionTip = false
 
